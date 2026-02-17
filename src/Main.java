1d091ad45c435eb2bf35c024b1c65a06665b75f8
MG <gmelina69@gmail.com>
2026-02-17 11:54:15 +0400

    Updated task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
 
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
+
+        System.out.println();
+        price = 21_100;
+        miles = service.calculate(price);
+        System.out.println(miles);
+
+        System.out.println();
+        price = 33_790;
+        miles = service.calculate(price);
+        System.out.println(miles);
+
+    }
 }
